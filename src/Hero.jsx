1611c6724f0ffc8b27fbc68e0cbee45aa57b1fe8
yigitbaris <72226361+yigitbaris@users.noreply.github.com>
2023-08-08 14:55:57 +0300

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,14 +5,10 @@ const Hero = () => {
     <section className='hero'>
       <div className='hero-center'>
         <div className='hero-title'>
-          <h1>Contentful CMS</h1>
-          {/* <p>
-            Umami chambray cupping typewriter woke, kogi hot chicken mlkshk
-            hoodie XOXO man bun coloring book pug tumeric 3 wolf moon.
-            Succulents af single-origin coffee forage biodiesel drinking vinegar
-            tilde. Cupping prism cray mumblecore shaman hoodie, hammock chambray
-            tattooed meh distillery bespoke.
-          </p> */}
+          <h1>React Projects</h1>
+         <p>
+           Projects are the most practical way to learn any language, and<span> React </span> is no exception. Solidify my knowledge of React by creating these cool projects.
+         </p>
         </div>
         <div className='img-container'>
           <img
